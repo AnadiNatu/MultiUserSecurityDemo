9f4ef1cf3e90442dda456223dcab5a574814504c
AnadiNatu <anadiaarti2001@gmail.com>
2025-12-19 12:30:07 +0530

    first commit - project created

---
+++ MultiUserSecurityDemo/src/main/java/com/example/MultiUserSecurityDemo/MultiUserSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.MultiUserSecurityDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiUserSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiUserSecurityDemoApplication.class, args);
+	}
+
+}
